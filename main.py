d2d26aa7148b910de9ffe1e8ccef68e11fed6a09
radnunes <sleepervoid.c@gmail.com>
2025-03-30 02:36:43 +0100

    implemented pygame groups

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     dt = 0
     
     print("Starting asteroids!")
@@ -18,9 +26,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+            
+        updatable.update(dt)
+        
         screen.fill("black")
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
